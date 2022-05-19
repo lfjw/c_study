6106b36aa736846e0fc188cd49d1a922a53b77ad
jw <bjfe@qq.com>
2022-05-19 14:25:52 +0800

    09 指针

---
+++ 09/demo1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 0;
+  printf("0x%x\n", &i);
+  printf("%p\n", &i);
+  return 0;
+}
---
+++ 09/demo3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  char ac[] = {0, 1, 2, 3, 4, -1};
+  char *p = &ac[0];
+
+  int i;
+  for (i = 0; i < sizeof(ac) / sizeof(ac[0]); i++)
+  {
+    printf("%d\n", ac[i]);
+  }
+
+  while (*p != -1)
+  {
+    printf("%d\n", *p++);
+  }
+
+  return 0;
+}
